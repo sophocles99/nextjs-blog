e67d7c089e365f2371c3983f21957decd82833fc
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-05 17:33:58 +0100

    display blog posts using getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,25 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm David and I'm learning Next.js</p>
+        <p>Hi, I'm David and I'm learning Next.js</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
