b6be500c040f748e108d23e11f06cccf176580b4
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-06 13:44:37 +0100

    add title and Date component using date-fns

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,11 @@
+import Head from "next/head";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
-  
+
   return {
     props: {
       postData,
@@ -22,11 +24,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
       {postData.title}
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
+
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
